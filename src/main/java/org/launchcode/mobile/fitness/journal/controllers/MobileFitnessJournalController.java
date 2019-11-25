674908949680b4f80c01fbadf053280552204da1
Lilian Molina <lilian.molina29@gmail.com>
2019-11-25 15:53:47 -0500

    created html and database

---
+++ src/main/java/org/launchcode/mobile/fitness/journal/controllers/MobileFitnessJournalController.java
@@ -0,0 +1,10 @@
+package org.launchcode.mobile.fitness.journal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping
+public class MobileFitnessJournalController {
+}
