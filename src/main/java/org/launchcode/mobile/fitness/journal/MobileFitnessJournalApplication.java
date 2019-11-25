d8c14c88d9129076b2edb332cde0a16563b58251
Lilian Molina <lilian.molina29@gmail.com>
2019-11-25 14:14:13 -0500

    initial commit

---
+++ src/main/java/org/launchcode/mobile/fitness/journal/MobileFitnessJournalApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.mobile.fitness.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileFitnessJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileFitnessJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/mobile/fitness/journal/MobileFitnessJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.mobile.fitness.journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileFitnessJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
