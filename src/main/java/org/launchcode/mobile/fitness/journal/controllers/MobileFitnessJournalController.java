4fb8edc0f827d51f63ad27bb306d4005635c6351
Lilian Molina <lilian.molina29@gmail.com>
2019-12-10 15:40:34 -0500

    created user registration

--- src/main/java/org/launchcode/mobile/fitness/journal/controllers/MobileFitnessJournalController.java
+++ src/main/java/org/launchcode/mobile/fitness/journal/controllers/MobileFitnessJournalController.java
@@ -5,18 +5,85 @@ import org.launchcode.mobile.fitness.journal.models.data.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 
 @Controller
 @RequestMapping
 public class MobileFitnessJournalController {
     @Autowired
-    private UserDao UserDao;
+    private UserDao userDao;
 
+    //===========THIS CODE DIRECTS TO HOMEPAGE===============
     @RequestMapping(value = "")
-
     public String index(Model model){
+
         return "homepage";
     }
+
+    //---------------------------------------------------------
+
+    //=================BEGINNING CODE FOR USER REGISTRATION================
+
+    @RequestMapping(method = RequestMethod.GET, value = "register")
+    public String register(Model model) {
+        model.addAttribute("title", "Register");
+        model.addAttribute(new User());
+        return "register";
+    }
+
+    @RequestMapping(value = "register", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid User user,
+                      Errors errors) {
+
+        model.addAttribute(user);
+
+        if (errors.hasErrors()) {
+            return "register";
+
+        }
+
+        userDao.save(user);
+
+        return "add";
+    }
+
+    //-----------------ENDING CODE FOR USER REGISTRATION---------------
+
+
+
+
+    //===============BEGINNING CODE FOR USER LOGIN=====================
+
+    @RequestMapping(method = RequestMethod.GET, value = "login")
+    public String login(Model model) {
+        model.addAttribute("title", "Log In");
+        /*model.addAttribute(new User());*/
+        return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(Model model, @RequestParam String username, @RequestParam String password) {
+
+        //check if username is not emtpy
+       //User user = userDao.findByUsername(username);
+        //if (password.equals(user.getPassword()))
+     // set cookie
+     // redirect to home page
+     return "home";
+/* else {
+     model.addAttribute("myerror", "Incorrect Login Info");
+     //user.setPassword("");
+     return "login";
+ }*/
+    }
+
+    //---------------ENDING CODE FOR USER LOGIN---------------------
+
 }
--- src/main/java/org/launchcode/mobile/fitness/journal/models/User.java
+++ src/main/java/org/launchcode/mobile/fitness/journal/models/User.java
@@ -1,8 +1,13 @@
 package org.launchcode.mobile.fitness.journal.models;
 
+import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User {
@@ -10,7 +15,66 @@ public class User {
     @GeneratedValue
     private int id;
 
-    public User(int id) {
-        this.id = id;
+    @NotNull
+    @Size(min= 5, max= 15, message = "Username must be between 5 and 15 characters")
+    private String username;
+
+
+    @NotNull
+    @Size(min=5, message = "Password must be at least 5 characters long")
+    private String password;
+
+    @Transient
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
+
+ /*   @OneToMany
+    @JoinColumn(name = "user_id")
+    private List<Location> location = new ArrayList<>();*/
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
+
+    public User() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+
+        this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() { return verifyPassword; }
+
+    public void setVerifyPassword(String verifyPassword) {
+
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword(){
+        if (password != null && verifyPassword != null && !password.equals(verifyPassword)) {
+            verifyPassword = null;
+        }
+
+
+    }
+
+/*    public List<Location> getLocation() {
+        return location;
+    }*/
 }
